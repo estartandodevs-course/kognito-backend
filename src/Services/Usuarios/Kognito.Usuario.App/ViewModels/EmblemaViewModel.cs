4ba8717628e91ede62aff947972b65fa614e5672
Miriam Bastos <miriamrbastos1@gmail.com>
2024-11-24 18:38:11 -0300

    feat(Usuario: App): Incui ViewModels de Usuario

---
+++ src/Services/Usuarios/Kognito.Usuario.App/ViewModels/EmblemaViewModel.cs
@@ -0,0 +1,22 @@
+using Kognito.Usuario.Domain;
+
+namespace Kognito.Usuario.App.ViewModels
+{
+    public class EmblemaViewModel
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime? DesbloqueadoEm { get; set; }
+
+        public EmblemaViewModel() { }
+
+        public EmblemaViewModel(Guid id, string nome, string descricao, DateTime? desbloqueadoEm)
+        {
+            Id = id;
+            Nome = nome;
+            Descricao = descricao;
+            DesbloqueadoEm = desbloqueadoEm;
+        }
+    }
+}
---
+++ src/Services/Usuarios/Kognito.Usuario.App/ViewModels/MetaViewModel.cs
@@ -0,0 +1,26 @@
+using Kognito.Usuario.Domain;
+
+namespace Kognito.Usuario.App.ViewModels
+{
+    public class MetaViewModel
+    {
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public bool Concluida { get; set; }
+        public DateTime CriadaEm { get; set; }
+        public DateTime? ConcluidaEm { get; set; }
+
+        public MetaViewModel() { }
+
+        public MetaViewModel(Guid id, string titulo, string descricao, bool concluida, DateTime criadaEm, DateTime? concluidaEm)
+        {
+            Id = id;
+            Titulo = titulo;
+            Descricao = descricao;
+            Concluida = concluida;
+            CriadaEm = criadaEm;
+            ConcluidaEm = concluidaEm;
+        }
+    }
+}
