05093bd8b03b15987876c61b64b09f73b5cdbb89
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-11-27 22:00:07 -0300

    feat(queries: Tarefas): Inclui queries do contexto de tarefas

---
+++ src/Services/Tarefas/Kognito.Tarefas.App/Queries/ITarefaQueries.cs
@@ -0,0 +1,11 @@
+﻿using Kognito.Tarefas.App.ViewModels;
+
+namespace Kognito.Tarefas.App.Queries;
+
+public interface ITarefaQueries
+{
+    Task<TarefaViewModel> ObterPorId(Guid id);
+    Task<IEnumerable<TarefaViewModel>> ObterPorTurma(Guid turmaId);
+    Task<IEnumerable<TarefaViewModel>> ObterPorAluno(Guid alunoId);
+    Task<IEnumerable<TarefaViewModel>> ObterTarefasComNotasPorTurma(Guid turmaId);
+}
