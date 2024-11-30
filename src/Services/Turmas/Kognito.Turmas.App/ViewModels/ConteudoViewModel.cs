1db7573c8c342a3a81a3199b397d77c7823c5229
Adriano Teixeira <adrianoteixeira1223@gmail.com>
2024-11-30 19:25:18 -0300

    feat(ViewModel: App) Inclui ConteudoViewModel

---
+++ src/Services/Turmas/Kognito.Turmas.App/ViewModels/ConteudoViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+using Kognito.Turmas.Domain;
+
+namespace Kognito.Turmas.App.ViewModels;
+
+public class ConteudoViewModel
+{
+    public Guid Id { get; set; }
+    public string Titulo { get; set; }
+    public string ConteudoDidatico { get; set; }
+
+    public static ConteudoViewModel Mapear(Conteudo conteudo)
+    {
+        return new ConteudoViewModel
+        {
+            Id = conteudo.Id,
+            Titulo = conteudo.Titulo,
+            ConteudoDidatico = conteudo.ConteudoDidatico
+        };
+    }
+}
