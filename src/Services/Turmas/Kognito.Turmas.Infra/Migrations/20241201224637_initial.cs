9f2782104f1b48695df5f3da70a7a2b112ac8bdc
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-12-01 19:48:59 -0300

    chore: Faz miggrations de turmas

---
+++ src/Services/Turmas/Kognito.Turmas.Infra/Migrations/20241201224637_initial.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kognito.Turmas.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Turmas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProfessorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProfessorNome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(500)", nullable: false),
+                    Materia = table.Column<string>(type: "varchar(100)", nullable: false),
+                    LinkAcesso = table.Column<string>(type: "varchar(200)", nullable: true),
+                    Cor = table.Column<int>(type: "int", nullable: false),
+                    Icones = table.Column<int>(type: "int", nullable: false),
+                    DataDeCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataDeAlteracao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Turmas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Conteudos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Titulo = table.Column<string>(type: "varchar(100)", nullable: false),
+                    ConteudoDidatico = table.Column<string>(type: "text", nullable: false),
+                    TurmaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DataDeCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataDeAlteracao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Conteudos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Conteudos_Turmas_TurmaId",
+                        column: x => x.TurmaId,
+                        principalTable: "Turmas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enturmamentos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AlunoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AlunoNome = table.Column<string>(type: "varchar(100)", nullable: false),
+                    TurmaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    DataDeCadastro = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataDeAlteracao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enturmamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Enturmamentos_Turmas_TurmaId",
+                        column: x => x.TurmaId,
+                        principalTable: "Turmas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Conteudos_TurmaId",
+                table: "Conteudos",
+                column: "TurmaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enturmamentos_TurmaId",
+                table: "Enturmamentos",
+                column: "TurmaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Conteudos");
+
+            migrationBuilder.DropTable(
+                name: "Enturmamentos");
+
+            migrationBuilder.DropTable(
+                name: "Turmas");
+        }
+    }
+}
