0f37c70e6edffc20eebc33684c45945f330c2128
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-12-02 17:10:55 -0300

    refactor: Remove validação do command e delega validação para inputModel

--- src/Services/Usuarios/Kognito.Usuarios.App/Commands/CriarMetaUsuarioCommand.cs
+++ src/Services/Usuarios/Kognito.Usuarios.App/Commands/CriarMetaUsuarioCommand.cs
@@ -5,32 +5,15 @@ namespace Kognito.Usuarios.App.Commands;
 
 public class CriarMetaCommand : Command
 {
+    public Guid UsuarioId { get; private set; }
     public string Titulo { get; private set; }
     public string Descricao { get; private set; }
 
-    public CriarMetaCommand(string titulo, string descricao)
+    public CriarMetaCommand(Guid usuarioId, string titulo, string descricao)
     {
+        UsuarioId = usuarioId;
         Titulo = titulo;
         Descricao = descricao;
     }
-
-    public override bool EstaValido()
-    {
-        ValidationResult = new CriarMetaValidation().Validate(this);
-        return ValidationResult.IsValid;
-    }
-
-    public class CriarMetaValidation : AbstractValidator<CriarMetaCommand>
-    {
-        public CriarMetaValidation()
-        {
-            RuleFor(x => x.Titulo)
-                .NotEmpty().WithMessage("O título da meta é obrigatório")
-                .MaximumLength(100).WithMessage("O título deve ter no máximo 100 caracteres");
-
-            RuleFor(x => x.Descricao)
-                .NotEmpty().WithMessage("A descrição da meta é obrigatória")
-                .MaximumLength(500).WithMessage("A descrição deve ter no máximo 500 caracteres");
-        }
-    }
+    
 }
