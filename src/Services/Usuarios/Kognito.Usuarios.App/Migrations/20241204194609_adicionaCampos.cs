5c5ab2596794d50767e81c6cedebdfcb7bcc13d5
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-12-04 19:24:49 -0300

    feat: faz migrations para incluir email de responsável e código de recuperação de senha

---
+++ src/Services/Usuarios/Kognito.Usuarios.App/Migrations/20241204194609_adicionaCampos.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kognito.Usuarios.App.Migrations
+{
+    /// <inheritdoc />
+    public partial class adicionaCampos : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CodigoPai",
+                table: "Usuarios",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CodigoRecuperacaoEmail",
+                table: "Usuarios",
+                type: "nvarchar(32)",
+                maxLength: 32,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ResponsavelEmail",
+                table: "Usuarios",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CodigoPai",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "CodigoRecuperacaoEmail",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "ResponsavelEmail",
+                table: "Usuarios");
+        }
+    }
+}
