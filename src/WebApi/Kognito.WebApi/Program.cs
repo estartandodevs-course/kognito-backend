6481c2d2f36a768e9b0403443585ef4e74f9b0d2
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-12-04 16:17:51 -0300

    chore: Remove texto do Program.cs

--- src/WebApi/Kognito.WebApi/Program.cs
+++ src/WebApi/Kognito.WebApi/Program.cs
@@ -3,8 +3,6 @@ using MediatR;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
