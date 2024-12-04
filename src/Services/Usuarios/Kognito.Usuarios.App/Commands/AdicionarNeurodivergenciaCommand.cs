be46b50b66c16ca8b9ce28bd6ef3f2a68e21417d
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-12-04 19:26:15 -0300

    feat: Command de para rota de inclusão de neurodivergência
    - Atualiza viewModel de usuário

---
+++ src/Services/Usuarios/Kognito.Usuarios.App/Commands/AdicionarNeurodivergenciaCommand.cs
@@ -0,0 +1,17 @@
+﻿using EstartandoDevsCore.Messages;
+
+namespace Kognito.Usuarios.App.Commands;
+
+public class AdicionarNeurodivergenciaCommand : Command
+{
+    public Guid UsuarioId { get; private set; }
+    public Guid CodigoPai { get; private set; }
+    public string Neurodivergencia { get; private set; }
+
+    public AdicionarNeurodivergenciaCommand(Guid usuarioId, Guid codigoPai, string neurodivergencia)
+    {
+        UsuarioId = usuarioId;
+        CodigoPai = codigoPai;
+        Neurodivergencia = neurodivergencia;
+    }
+}
--- src/Services/Usuarios/Kognito.Usuarios.App/ViewModels/UsuarioViewModel.cs
+++ src/Services/Usuarios/Kognito.Usuarios.App/ViewModels/UsuarioViewModel.cs
@@ -8,7 +8,6 @@ public class UsuarioViewModel
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Cpf { get; set; }
-    public Neurodivergencia? Neurodiversity { get; set; }
     public string Email { get; set; }
 
     public static UsuarioViewModel Mapear(Usuario usuario)
@@ -20,7 +19,6 @@ public class UsuarioViewModel
             Id = usuario.Id,
             Name = usuario.Nome,
             Cpf = usuario.Cpf.Numero,
-            Neurodiversity = usuario.Neurodivergencia.HasValue ? usuario.Neurodivergencia.Value : null,
             Email = usuario.Login?.Email?.Endereco,
         };
     }
