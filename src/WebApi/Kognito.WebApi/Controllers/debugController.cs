ebb52d857a57fd6bf99e2718e4cc192d250e349b
Afranio Caires <137647669+AfranioCaires@users.noreply.github.com>
2024-12-09 04:30:59 -0300

    feat(Debug controller): Cria um controller de debug para facilicar acesso aos hash gerados para email

---
+++ src/WebApi/Kognito.WebApi/Controllers/debugController.cs
@@ -0,0 +1,38 @@
+﻿using Kognito.Usuarios.App.Queries;
+
+namespace Kognito.WebApi.Controllers;
+
+using EstartandoDevsWebApiCore.Controllers;
+using Kognito.Usuarios.App.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/debug")]
+public class DebugController : MainController
+{
+    private readonly IUsuarioQueries _usuarioQueries;
+
+    public DebugController(IUsuarioQueries usuarioQueries)
+    {
+        _usuarioQueries = usuarioQueries;
+    }
+
+    [HttpGet("codes/{email}")]
+    public async Task<IActionResult> ObterCodigos(string email)
+    {
+        if (string.IsNullOrEmpty(email))
+        {
+            AdicionarErro("Email inválido");
+            return CustomResponse();
+        }
+
+        var codigoRecuperacao = await _usuarioQueries.ObterCodigoRecuperacaoPorEmail(email);
+        var codigoPai = await _usuarioQueries.ObterCodigoPaiPorEmail(email);
+
+        return CustomResponse(new
+        {
+            email,
+            codigoRecuperacao,
+            codigoPai
+        });
+    }
+}
